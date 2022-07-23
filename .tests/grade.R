b00854f2c80f6bf9ddc2b977fe88448df4f941f8
Alfredo Garbuno Iñigo <alfredo.garbuno@itam.mx>
2022-07-23 14:27:20 -0500

    Update grade.R

--- .tests/grade.R
+++ .tests/grade.R
@@ -1,3 +1,3 @@
-knitr::opts_chunk$set(purl=FALSE, quiet = TRUE)
-knitr::purl("assignment.Rmd",output=".tests/testthat/solutions.R")
+knitr::opts_chunk$set(purl=FALSE)
+knitr::purl("assignment.Rmd",output=".tests/testthat/solutions.R",quiet=TRUE)
 testthat::test_dir('.tests/testthat/')
