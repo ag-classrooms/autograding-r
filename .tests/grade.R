784946b87a16eb5db3e58eaf606a62123c73d2e3
agarbuno <alfredo.garbuno@itam.mx>
2022-07-23 14:25:27 -0500

    moved grader

--- .tests/grade.R
+++ .tests/grade.R
@@ -1,3 +1,3 @@
-knitr::opts_chunk$set(purl=FALSE)
+knitr::opts_chunk$set(purl=FALSE, quiet = TRUE)
 knitr::purl("assignment.Rmd",output=".tests/testthat/solutions.R")
 testthat::test_dir('.tests/testthat/')
