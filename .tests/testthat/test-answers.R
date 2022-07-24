bd7ae8b9cefa412cbc484d91a4bc36714f8237f7
Alfredo Garbuno Iñigo <alfredo.garbuno@itam.mx>
2022-07-23 22:13:43 -0500

    Update test-answers.R

--- .tests/testthat/test-answers.R
+++ .tests/testthat/test-answers.R
@@ -11,3 +11,11 @@ test_that("power works", {
 test_that("simulated function" ,{
   expect_equal(length(test(10)), 10)
 })
+
+test_that("simulated max" ,{
+  expect_lte(max(test(10)), 1)
+})
+
+test_that("simulated min" ,{
+  expect_gte(min(test(10)), 0)
+})
