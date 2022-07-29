be83d1b502c5182edb1d8dc8e4af0af137b6cb30
Alfredo Garbuno Iñigo <alfredo.garbuno@itam.mx>
2022-07-29 09:46:22 -0500

    Docker build (#9)

--- .tests/grade.R
+++ .tests/grade.R
@@ -1,5 +1,3 @@
-## getwd()
-library(testthat)
 knitr::opts_chunk$set(purl=FALSE)
 knitr::purl("assignment.Rmd",output=".tests/testthat/solutions.R",quiet=TRUE)
 testthat::test_dir('.tests/testthat/')
