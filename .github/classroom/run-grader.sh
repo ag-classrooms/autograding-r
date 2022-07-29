e01e2b0316a9ab31fd21455ed960fa7bf2eb014f
agarbuno <alfredo.garbuno@itam.mx>
2022-07-29 08:04:53 -0500

    test grader:beta

--- .github/classroom/run-grader.sh
+++ .github/classroom/run-grader.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run -v "local-directory:/home/rstudio/" agarbuno/grader:4.2.1 Rscript .tests/grade.R
+docker run -v "local-directory:/home/rstudio/" agarbuno/grader:beta Rscript .tests/grade.R
