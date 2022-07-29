86dafc4fcdb9198315d307204a89554282e09747
agarbuno <alfredo.garbuno@itam.mx>
2022-07-29 09:28:42 -0500

    make sure all packages are there

--- .tests/grade.R
+++ .tests/grade.R
@@ -1,5 +1,4 @@
-## getwd()
-library(testthat)
+renv::restore()
 knitr::opts_chunk$set(purl=FALSE)
 knitr::purl("assignment.Rmd",output=".tests/testthat/solutions.R",quiet=TRUE)
 testthat::test_dir('.tests/testthat/')
